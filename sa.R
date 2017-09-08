b7c0258c02c52e2efa7e28f4eb18baf7619c8ff7
Jailsonrs <jailson@sitesoft.com.br>
2017-09-08 05:27:25 -0300

    Insersão do algoritmo SA

---
+++ sa.R
@@ -0,0 +1,30 @@
+sa <- function(fun, N, rho, t, theta){
+y = NULL
+theta.old = theta
+repeat{
+s = 0
+for(k in 1:N){
+f.old = fun(theta.old)
+theta.new = runif(1,-1,1) + theta.old
+f.new = fun(theta.new)
+if(f.new<f.old){
+theta.star = theta.new
+s = s+1
+}else{
+u = runif(1)
+b = (u <= exp(-(f.new-f.old)/t))
+theta.star = theta.new * b + theta.old * (1-b)
+s = s+b
+}
+theta.old = theta.star
+y = c(y, theta.star)
+}
+write(y, file = "anneal.result", ncol = 1, append = T)
+r = y[length(y)]
+y = NULL
+if(s==0) break
+t = rho*t
+if(t<.1) break
+}
+return(r)
+}
--- server.R
+++ server.R
@@ -1,9 +1,11 @@
 library(shiny)
 library(tidyverse)
 library(DT)
+source("sa.R")
 function(input, output){
   output$tab1<- DT::renderDataTable({
     DT::datatable(mtcars)
  })
+
 
 }
