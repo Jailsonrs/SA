c92de3ef76cafffa9571d49deea257a2c44ff2fc
Jailsonrs <jailson@sitesoft.com.br>
2017-09-23 08:08:36 -0300

    Melhoramento do cabeçalho do HTML e da barra lateral.

--- server.R
+++ server.R
@@ -1,30 +1,40 @@
 library(shiny)
+library(plotly)
+
 library(tidyverse)
+##install.packages("shinycssloaders")
+library(shinycssloaders)
 library(DT)
+library(highcharter)
+##install.packages("highcharter")
 library(ggthemes)
+library(ggplot2)
+library(magrittr)
 source("sa.R")
 tema <- theme(
-  panel.background = element_blank(),
-  panel.grid.major = element_line(colour="red", color="red")
+  panel.background = element_rect(fill="azure2"),
+  panel.grid.minor = element_line(color="red"),
+  axis.line = element_line(size=1,linetype=2, arrow=1)
+
 )
+
+
 function(input, output){
+  data(mtcars)
   output$reativo <- renderPlotly({
-    p <- ggplot(mtcars,aes(mpg, hp, fill=hp) )+
-      geom_point()+
+    p <-ggplot(mtcars,aes(mpg, hp))+
+      geom_point(col="deepskyblue4")+
       ggtitle("Diagrama de Dispersao")+
-      theme_bw()
-    gp <- ggplotly() %>% config(collaborate=FALSE, cloud=FALSE, displaylogo=FALSE)
-    gp <- layout(gp, margin=list(t = 75))
+      stat_smooth(method=lm,se=F)+
+      tema
+    
+    gp <- ggplotly() %>% config(collaborate=FALSE, cloud=FALSE, displaylogo=FALSE, modeBarButtonsToRemove=c("select2d", "sendDataToCloud", "pan2d", "resetScale2d", "hoverClosestCartesian", "hoverCompareCartesian", "lasso2d", "zoomIn2d", "zoomOut2d"))
+    gp <- layout(gp, margin=list(t = 100))
     gp
   })
-
   output$tab1 <- renderDataTable({
     DT::datatable(mtcars)
   })
 }
-
-
-
-
 
 
--- ui.R
+++ ui.R
@@ -1,13 +1,35 @@
 library(shiny)
 library(htmltools)
+options(spinner.size=1)
 htmlTemplate("index.html",
              button = actionButton(inputId = "teste", "Busca"),
              slider = sliderInput(inputId = "TESTE", "teste",min=0 ,max=10, 1, step=0.2),
              textinpt = textInput("teste","teste","Entre com a busca"),
-             tab1 = DT::dataTableOutput("tab1"),
-             seletor = selectInput("seletor", "selecione", choices=unique(colnames(mtcars))),
-             pizza = plotlyOutput("reativo"),
-             grafico2 = plotlyOutput("g2")
+             tab1 = withSpinner(DT::dataTableOutput("tab1"), type=4),
+             seletor = selectInput("seletor", "selecione", choices=unique(colnames(mtcars)), width ="40px"),
+             pizza = withSpinner(plotlyOutput("reativo", height = "299px"), type=6),
+             grafico2 = withSpinner(plotlyOutput("g2", width = "199px"), type=6),
+             seletor2 = selectInput("seletorVa", "Selecione a variável de interesse", choices = unique(colnames(mtcars)))
              )
 
+
+
+
+
+
+
+## FcustomCSS <- function (){
+
+ ## customCSS <- function (theme = NULL) {
+  
+ ##   htmlDependency("custom", "1.0", src=c(href = "shared/Apps", file = system.file("www/shared/Apps", package ="shiny")),
+ ##                  stylesheet ="style.css")
+
+
+ ## }
+
+ ## customCSS2 <- function (theme = NULL){
+ ##   htmlDependency("ccs", "1.0", src = c(href = "shiny"), stylesheet = "www/style.css")
+ ## }
+## customCSS2()
 
